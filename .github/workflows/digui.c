9f446f2ea0cde4509d19f6e7141cb4a0a7e36336
wazran <56401970+wazran@users.noreply.github.com>
2020-11-24 13:33:46 +0800

    Update digui.c

--- .github/workflows/digui.c
+++ .github/workflows/digui.c
@@ -1,7 +1,25 @@
 #inclode<stdio.h>
+
+void oshu(int o);
 int man ()
 {
- 
- 
+ int number=0;
+ scanf("scanf%d",&number);
+ for(;number=1;){
+  if(void oshu()==-1){
+   number *= 3
+  }else{
+   number /= 2
+  } 
+ }
  return 0;
  }
+void oshu(int o)
+{
+ int i=0 ;
+ if(i%2){
+  return i ;
+ }else {
+  return -1;
+ }
+}
